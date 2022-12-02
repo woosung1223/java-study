8d7fe523b3274c768745c317de05f29883cb23aa
woosung1223 <dntjd991223@naver.com>
2022-12-02 14:13:06 +0900

    컬렉션이 아닌 원시형에 대해 generic 사용해보기

---
+++ src/GenericType.java
@@ -0,0 +1,17 @@
+public class GenericType<T> {
+    private T data;
+
+    public void setter(T data) {
+        this.data = data;
+    }
+
+    public T getter() {
+        return this.data;
+    }
+
+    public void doSomeThing() {
+        data.hashCode();
+        // data는 타입이 정해져 있지 않기에 Object 메소드만 사용 가능한 것으로 보임.
+        // 반면 List 등은 추상 클래스이기 때문에 Object 이외에도 List의 메소드도 사용 가능.
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,22 @@
 
 public class Main {
     public static void main(String[] args) {
+//        genericListPractice();
+        genericTypePractice();
+    }
+
+    private static void genericListPractice() {
         GenericList<String> stringList = new GenericList<>();
         stringList.pushElement("123");
         stringList.pushElement("456");
 
         System.out.println(stringList.popElement());
         System.out.println(stringList.popElement());
+    }
+
+    private static void genericTypePractice() {
+        GenericType<Integer> integerType = new GenericType<>();
+        integerType.setter(123);
+        System.out.println(integerType.getter());
     }
 }
