69e1925bf19e69cc23a1398b4a8d3a2233918414
woosung1223 <dntjd991223@naver.com>
2023-01-20 21:55:00 +0900

    상속 테스트

--- src/Main.java
+++ src/Main.java
@@ -1,47 +1,39 @@
-import generic.*;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-//        genericListPractice();
-//        genericTypePractice();
-//        fruitPractice();
-        juicePractice();
+        Main test = new Main();
+        test.start();
     }
 
-    private static void genericListPractice() {
-        GenericList<String> stringList = new GenericList<>();
-        stringList.pushElement("123");
-        stringList.pushElement("456");
+    public void start() {
+        Child child = new Child(1, 2);
 
-        System.out.println(stringList.popElement());
-        System.out.println(stringList.popElement());
+        child.add();
+        child.subtract();
+    }
+    
+    public class Parent {
+        private int x, y;
+
+        public Parent(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        public void add() {
+            System.out.println(x + y);
+        }
+
+        public void subtract() {
+            System.out.println(x - y);
+        }
     }
 
-    private static void genericTypePractice() {
-        GenericType<Integer> integerType = new GenericType<>();
-        integerType.setter(123);
-        System.out.println(integerType.getter());
-    }
 
-    private static void fruitPractice() {
-        FruitBox<Grape> grapeTest = new FruitBox<>();
-        grapeTest.addFruit(new Grape());
-        grapeTest.addFruit(new Grape());
-        grapeTest.printBox();
-
-        FruitBox<Apple> appleTest = new FruitBox<>();
-        appleTest.addFruit(new Apple());
-        appleTest.addFruit(new Apple());
-        appleTest.printBox();
-
-//        generic.FruitBox<Integer> integerTest = new generic.FruitBox<Integer>(); // ERROR. Fruit의 자손만 타입이 될 수 있다.
-    }
-
-    private static void juicePractice() {
-        FruitBox<Grape> grapeTest = new FruitBox<>();
-        grapeTest.addFruit(new Grape());
-        grapeTest.addFruit(new Grape());
-
-        Juice.makeJuice(grapeTest);
+    public class Child extends Parent {
+        public Child(int x, int y) {
+            super(x, y);
+        }
     }
 }
